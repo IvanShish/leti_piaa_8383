54451602349ce67fd8a6c143812bd3e1bd6a8be3
Ivan <rayman.4@list.ru>
2020-04-09 10:53:55 +0300

    lab4 ver2

--- Shishkin/lab4/Source/PIAA_LR4_CYCLIC_SHIFT.cpp
+++ Shishkin/lab4/Source/PIAA_LR4_CYCLIC_SHIFT.cpp
@@ -1,110 +1,109 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-int KMPSearch(string& pat, string& text);    //КМП
-void prefixFunc(string pat, int M, vector<int>& pi); // Префикс функция
+int KMPSearch(const std::string& pat, std::string& text);    //КМП
+void prefixFunc(const std::string& pat, int lenOfPat, std::vector<int>& pi); // Префикс-функция
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    string pattern;    //шаблон (строка B)
-    string text;       //текст  (строка A)
-    cin >> text;
-    cin >> pattern;
+    std::string pattern;    //шаблон (строка B)
+    std::string text;       //текст  (строка A)
+    std::cin >> text;
+    std::cin >> pattern;
 
     int answer = KMPSearch(pattern, text);
 
-    cout << "-----------------------------------------------" << endl;
-    cout << "Индекс сдвига = " << answer << endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << "Индекс сдвига = " << answer << std::endl;
+    system("pause");
     return 0;
 }
 
 
-int KMPSearch(string& pat, string& text) {
-    cout << "Определение, является ли строка \"" << text << "\" циклическим сдвигом строки \"" << pat << "\"" << endl;
-    int M = pat.length();    //длина шаблона
-    int N = text.length();   //длина текста
+int KMPSearch(const std::string& pat, std::string& text) {
+    std::cout << "Определение, является ли строка \"" << text << "\" циклическим сдвигом строки \"" << pat << "\"" << std::endl;
+    int lenOfPat = pat.length();    //длина шаблона
+    int lenOfText = text.length();   //длина текста
 
-    if (M != N) {   //если длина текста и шаблона не равны, то возвращается -1
-        cout << "Длина текста и шаблона не равны" << endl;
+    if (lenOfPat != lenOfText) {   //если длина текста и шаблона не равны, то возвращается -1
+        std::cout << "Длина текста и шаблона не равны" << std::endl;
         return -1;
     }
     else if (pat == text) {
-        cout << "Строки идентичны" << endl;
+        std::cout << "Строки идентичны" << std::endl;
         return 0;
     }
 
     text += text;
-    N = text.length(); //новое значение длины текста
-    vector <int> pi(M, 0);   //длина максимального префикса строки, совпадающего с его суффиксом
+    lenOfText = text.length(); //новое значение длины текста
+    std::vector <int> pi(lenOfPat, 0);   //длина максимального префикса строки, совпадающего с его суффиксом
 
     int i = 0; //индекс для перемещения по строке text
     int j = 0; //индекс для перемещения по строке pattern
 
-    prefixFunc(pat, M, pi);   //вычисление массива pi для строки pat
+    prefixFunc(pat, lenOfPat, pi);   //вычисление массива pi для строки pat
 
-    cout << "Выполнение алгоритма Кнута-Морриса-Пратта..." << endl;
-    while (i < N) {
-        cout << "Индекс i - " << i << ", индекс j - " << j << endl;
+    std::cout << "Выполнение алгоритма Кнута-Морриса-Пратта..." << std::endl;
+    while (i < lenOfText) {
+        std::cout << "Индекс i - " << i << ", индекс j - " << j << std::endl;
 
         if (pat[j] == text[i]) {  //если j-ый символ шаблона равен i-му символу текста 
-            cout << "i-ый символ текста совпадает с j-ым символом шаблона, поэтому увеличиваем i и j на 1" << endl;
+            std::cout << "i-ый символ текста совпадает с j-ым символом шаблона, поэтому увеличиваем i и j на 1" << std::endl;
             j++;     //j увеличивается только когда находится сходство
             i++;
-            cout << endl << "Символ текста '" << text[i] << "': " << endl;
+            std::cout << std::endl << "Символ текста '" << text[i] << "': " << std::endl;
         }
 
-        if (j == M) {    //если j равен длине шаблона, то найдено вхождение шаблона в текст
-            cout << "Индекс i - " << i << ", индекс j - " << j << endl;
-            cout << "!!!Найдено вхождение шаблона в текст" << endl;
+        if (j == lenOfPat) {    //если j равен длине шаблона, то найдено вхождение шаблона в текст
+            std::cout << "Индекс i - " << i << ", индекс j - " << j << std::endl;
+            std::cout << "!!!Найдено вхождение шаблона в текст" << std::endl;
             return i - j;
         }
 
-        else if (i < N && pat[j] != text[i]) {  //если j-ый символ шаблона не равен символу текста
-            cout << "i-ый символ текста не совпадает с j-ым символом шаблона";
+        else if (i < lenOfText && pat[j] != text[i]) {  //если j-ый символ шаблона не равен символу текста
+            std::cout << "i-ый символ текста не совпадает с j-ым символом шаблона";
 
             if (j != 0) {
-                cout << ". Теперь j принимает значение " << pi[j - 1] << endl;
+                std::cout << ". Теперь j принимает значение " << pi[j - 1] << std::endl;
                 j = pi[j - 1];
             }
 
             else {
-                cout << ", j = 0, следовательно, производится увеличение i на 1" << endl;
+                std::cout << ", j = 0, следовательно, производится увеличение i на 1" << std::endl;
                 i++;
-                cout << endl << "Символ текста '" << text[i] << "': " << endl;
+                std::cout << std::endl << "Символ текста '" << text[i] << "': " << std::endl;
             }
         }
     }
 
-    cout << "Строка не является циклическим сдвигом" << endl;
+    std::cout << "Строка не является циклическим сдвигом" << std::endl;
     return -1;
 }
 
-void prefixFunc(string pat, int M, vector<int>& pi) {
-    cout << "Заполнение массива pi..." << endl;
-    for (int i = 1; i < M; i++) {   //вычисление массива пи от i := 1 до i := M-1
+void prefixFunc(const std::string& pat, int lenOfPat, std::vector<int>& pi) {
+    std::cout << "Заполнение массива pi..." << std::endl;
+    for (int i = 1; i < lenOfPat; i++) {   //вычисление массива пи от i := 1 до i := M-1
         int j = pi[i - 1];  //для подсчёта текущего значения pi[i] заводится переменная j, обозначающая длину текущего рассматриваемого образца
 
         while (j > 0 && pat[i] != pat[j]) {   //сравнение символов pat[i] и pat[j]
-            cout << "Обработка символов '" << pat[i] << "' и '" << pat[j] << "'..." << endl;
+            std::cout << "Обработка символов '" << pat[i] << "' и '" << pat[j] << "'..." << std::endl;
             j = pi[j - 1];
         }
 
         if (pat[i] == pat[j]) {  //если символы pat[i] и pat[j] совпадают, то pi[i] = j + 1
-            cout << "Суффикс '" << pat[i] << "' и префикс " << pat[j] << "' равны, pi[" << i << "] увеличивается на 1" << endl;
+            std::cout << "Суффикс '" << pat[i] << "' и префикс " << pat[j] << "' равны, pi[" << i << "] увеличивается на 1" << std::endl;
             ++j;
         }
 
-        cout << "pi[" << i << "] = " << j << endl;
+        std::cout << "pi[" << i << "] = " << j << std::endl;
         pi[i] = j;
     }
 
-    cout << "Итоговая префикс-функция:" << endl << "{ ";
+    std::cout << "Итоговая префикс-функция:" << std::endl << "{ ";
     for (int i = 0; i < pi.size(); i++) {
-        if (i == 0) cout << pi[i];
-        else cout << ", " << pi[i];
+        if (i == 0) std::cout << pi[i];
+        else std::cout << ", " << pi[i];
     }
-    cout << " }" << endl;
-    cout << "-----------------------------------------------" << endl;
+    std::cout << " }" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
 }
--- Shishkin/lab4/Source/PIAA_LR4_KMP.cpp
+++ Shishkin/lab4/Source/PIAA_LR4_KMP.cpp
@@ -1,113 +1,112 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-void KMPSearch(string pat, vector <int>& answer); // Выводит индексы вхождения строки pat в текст, который посимвольно вводится в этой функции
-void prefixFunc(string pat, int M, vector<int>& pi); // Префикс функция
+void KMPSearch(const std::string& pat, std::vector <int>& answer); // Выводит индексы вхождения строки pat в текст, который посимвольно вводится в этой функции
+void prefixFunc(const std::string& pat, int lenOfPat, std::vector<int>& pi); // Префикс-функция
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    string pattern;    //шаблон
-    cin >> pattern;
+    std::string pattern;    //шаблон
+    std::cin >> pattern;
     getchar();   //считывание символа '\n'
 
-    vector <int> answer;
+    std::vector <int> answer;
     KMPSearch(pattern, answer);
 
-    cout << "-----------------------------------------------" << endl;
-    cout << "Индексы начал вхождений шаблона в текст:" << endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << "Индексы начал вхождений шаблона в текст:" << std::endl;
     if (answer.size() == 0) {
-        cout << "Вхождений не было найдено, ответ: -1" << endl;
+        std::cout << "Вхождений не было найдено, ответ: -1" << std::endl;
     }
     else {
         for (int i = 0; i < answer.size(); i++) {
-            if (i == 0) cout << answer[i];
-            else cout << "," << answer[i];
+            if (i == 0) std::cout << answer[i];
+            else std::cout << "," << answer[i];
         }
     }
-    cout << endl;
+    std::cout << std::endl;
+    system("pause");
     return 0;
 }
 
 
-void KMPSearch(string pat, vector <int>& answer) {
+void KMPSearch(const std::string& pat, std::vector <int>& answer) {
     char sym = NULL;  //для посимвольного считывания текста
-    int M = pat.length();    //длина шаблона
+    int lenOfPat = pat.length();    //длина шаблона
 
-    vector <int> pi(M, 0);   //длина максимального префикса строки, совпадающего с его суффиксом
+    std::vector <int> pi(lenOfPat, 0);   //длина максимального префикса строки, совпадающего с его суффиксом
 
     int i = 0; //индекс для перемещения по тексту
     int j = 0; //индекс для перемещения по строке pattern
 
-    sym = cin.get();  //считывание первого символа
+    sym = std::cin.get();  //считывание первого символа
 
-    prefixFunc(pat, M, pi);   //вычисление массива pi для строки pat
+    prefixFunc(pat, lenOfPat, pi);   //вычисление массива pi для строки pat
 
-    cout << "Выполнение алгоритма Кнута-Морриса-Пратта..." << endl;
-    if (sym != '\n' && sym != EOF) cout << "Символ текста '" << sym << "': " << endl;
+    std::cout << "Выполнение алгоритма Кнута-Морриса-Пратта..." << std::endl;
+    if (sym != '\n' && sym != EOF) std::cout << "Символ текста '" << sym << "': " << std::endl;
     while (sym != '\n' && sym != EOF) {
-        cout << "Индекс i - " << i << ", индекс j - " << j << endl;
+        std::cout << "Индекс i - " << i << ", индекс j - " << j << std::endl;
 
         if (pat[j] == sym) {  //если j-ый символ шаблона равен i-му символу текста 
-            cout << "Символ текста совпадает с j-ым символом шаблона, поэтому увеличиваем i и j на 1" << endl;
+            std::cout << "Символ текста совпадает с j-ым символом шаблона, поэтому увеличиваем i и j на 1" << std::endl;
             j++;     //j увеличивается только когда находится сходство
             i++;
-            sym = cin.get();
-            if (sym != '\n' && sym != EOF) cout << endl << "Символ текста '" << sym << "': " << endl;
+            sym = std::cin.get();
+            if (sym != '\n' && sym != EOF) std::cout << std::endl << "Символ текста '" << sym << "': " << std::endl;
         }
 
-        if (j == M) {    //если j равен длине шаблона, то найдено вхождение шаблона в текст
-            cout << "Индекс i - " << i << ", индекс j - " << j << endl;
-            cout << "!!!Найдено вхождение шаблона в текст. Индекс вхождения - " << i - j << endl;
+        if (j == lenOfPat) {    //если j равен длине шаблона, то найдено вхождение шаблона в текст
+            std::cout << "Индекс i - " << i << ", индекс j - " << j << std::endl;
+            std::cout << "!!!Найдено вхождение шаблона в текст. Индекс вхождения - " << i - j << std::endl;
             answer.push_back(i - j);
-            cout << "Сброс j до значения " << pi[j - 1] << endl << endl;
+            std::cout << "Сброс j до значения " << pi[j - 1] << std::endl << std::endl;
             j = pi[j - 1];
         }
 
         else if (pat[j] != sym) {  //если j-ый символ шаблона не равен символу текста
-            cout << "Символ текста не совпадает с j-ым символом шаблона";
+            std::cout << "Символ текста не совпадает с j-ым символом шаблона";
 
             if (j != 0) {
-                cout << ". Теперь j принимает значение " << pi[j - 1] << endl;
+                std::cout << ". Теперь j принимает значение " << pi[j - 1] << std::endl;
                 j = pi[j - 1];
             }
 
             else {
-                cout << ", j = 0, следовательно, производится увеличение i на 1" << endl;
+                std::cout << ", j = 0, следовательно, производится увеличение i на 1" << std::endl;
                 i = i + 1;
-                sym = cin.get();
-                if (sym != '\n' && sym != EOF) cout << endl << "Символ текста '" << sym << "': " << endl;
+                sym = std::cin.get();
+                if (sym != '\n' && sym != EOF) std::cout << std::endl << "Символ текста '" << sym << "': " << std::endl;
             }
         }
     }
 
 }
 
-void prefixFunc(string pat, int M, vector<int>& pi) {
-    cout << "Заполнение массива pi..." << endl;
-    for (int i = 1; i < M; i++) {   //вычисление массива пи от i := 1 до i := M-1
+void prefixFunc(const std::string& pat, int lenOfPat, std::vector<int>& pi) {
+    std::cout << "Заполнение массива pi..." << std::endl;
+    for (int i = 1; i < lenOfPat; i++) {   //вычисление массива пи от i := 1 до i := M-1
         int j = pi[i - 1];  //для подсчёта текущего значения pi[i] заводится переменная j, обозначающая длину текущего рассматриваемого образца
 
         while (j > 0 && pat[i] != pat[j]) {   //сравнение символов pat[i] и pat[j]
-            cout << "Обработка символов '" << pat[i] << "' и '" << pat[j] << "'..." << endl;
+            std::cout << "Обработка символов '" << pat[i] << "' и '" << pat[j] << "'..." << std::endl;
             j = pi[j - 1];
         }
 
         if (pat[i] == pat[j]) {  //если символы pat[i] и pat[j] совпадают, то pi[i] = j + 1
-            cout << "Суффикс '" << pat[i] << "' и префикс " << pat[j] << "' равны, pi[" << i << "] увеличивается на 1" << endl;
+            std::cout << "Суффикс '" << pat[i] << "' и префикс " << pat[j] << "' равны, pi[" << i << "] увеличивается на 1" << std::endl;
             ++j;
         }
 
-        cout << "pi[" << i << "] = " << j << endl;
+        std::cout << "pi[" << i << "] = " << j << std::endl;
         pi[i] = j;
     }
 
-    cout << "Итоговая префикс-функция:" << endl << "{ ";
+    std::cout << "Итоговая префикс-функция:" << std::endl << "{ ";
     for (int i = 0; i < pi.size(); i++) {
-        if (i == 0) cout << pi[i];
-        else cout << ", " << pi[i];
+        if (i == 0) std::cout << pi[i];
+        else std::cout << ", " << pi[i];
     }
-    cout << " }" << endl;
-    cout << "-----------------------------------------------" << endl;
+    std::cout << " }" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
 }
